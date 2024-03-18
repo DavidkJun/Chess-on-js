6013d064067e8f943ddb78fe2b52aba908a5cc14
Davyd Krykota <krikotadavid957@gmail.com>
2024-03-18 17:29:16 +0200

    Added start pieces

--- app.js
+++ app.js
@@ -6,3 +6,14 @@ const width = 8
 
 let playerTurn = 'black';
 playerDetails.textContent = 'black'
+
+const startPieces = [
+    Rook, Knight, Bishop, Queen, King, Bishop, Knight, Rook,
+    Pawn, Pawn, Pawn, Pawn, Pawn, Pawn, Pawn, Pawn,
+    '', '', '', '', '', '', '', '',
+    '', '', '', '', '', '', '', '',
+    '', '', '', '', '', '', '', '',
+    '', '', '', '', '', '', '', '',
+    Pawn, Pawn, Pawn, Pawn, Pawn, Pawn, Pawn, Pawn,
+    Rook, Knight, Bishop, King, Queen, Bishop, Knight, Rook,
+]
