4bdf599b7a56b6a407b958d86c02ee90ef7b3687
Davyd Krykota <krikotadavid957@gmail.com>
2024-03-18 17:26:52 +0200

    Added basic details

---
+++ app.js
@@ -0,0 +1,8 @@
+const gameBoard = document.querySelector("#gameboard");
+const playerDetails = document.querySelector("#player");
+const infoDisplay = document.querySelector("#info-display");
+const err = document.querySelector("#err");
+const width = 8
+
+let playerTurn = 'black';
+playerDetails.textContent = 'black'
